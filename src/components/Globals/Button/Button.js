8e5af3b36185a4da62dfb16b6295456a59bc5e84
EstefaMH <estefymoncaleano@gmail.com>
2023-07-07 09:59:43 -0500

    add Components -> Button, Header and images

--- src/components/Globals/Button/Button.js
+++ src/components/Globals/Button/Button.js
@@ -2,7 +2,7 @@ import React from "react";
 import './Button.css';
 
 function Button(props) {
-  return <button>{props.text}</button>;
+  return <button className="global-button">{props.text}</button>;
 }
 
 export default Button;
