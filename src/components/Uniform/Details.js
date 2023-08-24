ff1f8d69467f8d1858d72ab6d8dbe66e8bd2865b
Yany954 <yanygonzalezyepez@gmail.com>
2023-08-24 14:45:17 -0400

    detail uniform responsive

--- src/components/Uniform/Details.js
+++ src/components/Uniform/Details.js
@@ -1,92 +1,88 @@
-import React from 'react';
-import Title from '../Globals/Title/Title';
-import './Uniform.css';
-import Uniforme from '../../assets/img/UniformeEAN.svg';
+import React from "react";
+import Title from "../Globals/Title/Title";
+import "./Uniform.css";
+import Uniforme from "../../assets/img/UniformeEAN.svg";
 
 function Details() {
   return (
-    <section className='details'>
-      <div className='section-image-tallas'>
-      <div className='section'>
-        <div className='image'>
-          <article className='round-card'>
-            <aside className='round-card-vector'>
+    <section className="details">
+      <div className="section-image-tallas">
+        <div className="section">
+          <div className="image">
+            <article className="round-card">
+              <aside className="round-card-vector">
                 <img src={Uniforme} />
-            </aside>
-          </article>
-        </div>
-        <div className='price'>
-          <span className='currency'>$</span>
-          <span className='amount'>150.000</span>
-        </div>
-      </div>
-      <div className='section-image-details'>
-      <div className='section'>
-        <div className='size'>
-          <Title title='Tallas' />
-          <div className='size-cards'>
-            <div class="card">
-              <h1>XS</h1>
-            </div>
-            <div class="card">
-              <h1>S</h1>
-            </div>
-            <div class="card">
-              <h1>M</h1>
-            </div>
-            <div class="card">
-              <h1>L</h1>
-            </div>
-            <div class="card">
-              <h1>XL</h1>
-            </div>
-            <div class="card">
-              <h1>XXL</h1>
-            </div>
-            
+              </aside>
+            </article>
           </div>
-          
-          
-          <a className='section-guia-tallas' href="">&gt;&gt;Guía de tallas</a>
-        </div>
-        </div>
-        </div>
-        <div className='section'>
-        <div className='uniform'>
-          <Title title='Uniforme' />
-          <div className='square-cards'>
-          <div className='square-card'>
-            <section className='square-card-img' >
-                <img src={Uniforme} alt='uniforme' />
-            </section>
-            </div>
-            <div className='square-card'>
-            <section className='square-card-img' >
-                <img src={Uniforme} alt='uniforme' />
-            </section>
-            </div>
-            </div>
-            </div>
-        </div>
-        
-      <div className='section'>
-      <div className='section-image-details'>
-        <div className='section-btns'>
-          
-          <button className='btn'>+</button>
-          <input type='number' className='quantity' />
-          <button className='btn'>-</button>
-        </div>
-        <button className='add-btn'>Agregar</button>
-        <div className='total-price'>
-        <Title title='Total: ' />
-        <span className='currency'>$</span>
-          <span className='amount'>550.000</span>
+          <div className="price">
+            <span className="currency">$</span>
+            <span className="amount">150.000</span>
+          </div>
         </div>
       </div>
+      <div className="section-image-details">
+        <div className="section">
+          <div className="size">
+            <Title title="Tallas" />
+            <div className="size-cards">
+              <div class="card">
+                <h1>XS</h1>
+              </div>
+              <div class="card">
+                <h1>S</h1>
+              </div>
+              <div class="card">
+                <h1>M</h1>
+              </div>
+              <div class="card">
+                <h1>L</h1>
+              </div>
+              <div class="card">
+                <h1>XL</h1>
+              </div>
+              <div class="card">
+                <h1>XXL</h1>
+              </div>
+
+              <a className="section-guia-tallas" href="">
+                &gt;&gt;Guía de tallas
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="section">
+          <div className="uniform">
+            <Title title="Uniforme" />
+            <div className="square-cards">
+              <div className="square-card">
+                <section className="square-card-img">
+                  <img src={Uniforme} alt="uniforme" />
+                </section>
+              </div>
+              <div className="square-card">
+                <section className="square-card-img">
+                  <img src={Uniforme} alt="uniforme" />
+                </section>
+              </div>
+            </div>
+          </div>
+
+          <div className="section">
+            <div className="section-btns">
+              <button className="btn">+</button>
+              <input type="text" className="quantity" />
+              <button className="btn">-</button>
+            </div>
+            <button className="add-btn">Agregar</button>
+            <div className="total-price">
+              <Title title="Total: " />
+              <span className="currency">$</span>
+              <span className="amount">550.000</span>
+            </div>
+          </div>
+        </div>
       </div>
-      </div>
-      
     </section>
   );
 }
--- src/pages/Uniform.js
+++ src/pages/Uniform.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Header from '../components/Globals/Header/Header'
 import WhatsApp from '../components/Globals/WhatsApp/WhatsApp'
 import Details from '../components/Uniform/Details'
+import Footer from '../components/Globals/Footer/Footer';
 
 function Uniform() {
   return (
     <div className='container-background'>
     <Header />
     <Details />
+    <Footer />
     <WhatsApp />
     </div>
   )
