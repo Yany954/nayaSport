ccc6ba25ed6a735951f0f41e4f9b7a675ed3eab8
unknown <johanfgomez@ucompensar.edu.co>
2023-09-03 10:06:34 -0500

    Login CSS

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios'; 
 
-import './Login.css'; 
-
+import './Login.css';
 export default function Login() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
@@ -28,22 +27,24 @@ export default function Login() {
   return (
     <div className='login-container'>
       <form onSubmit={handleSubmit}>
-        <div>
+        <div className='input-container'>
           <label htmlFor="username">Usuario</label>
           <input
             type="text"
             id="username"
             value={username}
             onChange={(e) => setUsername(e.target.value)}
+            placeholder="Usuario" 
           />
         </div>
-        <div>
+        <div className='input-container'>
           <label htmlFor="password">Contraseña</label>
           <input
             type="password"
             id="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
+            placeholder="Contraseña" 
           />
         </div>
         <div>
