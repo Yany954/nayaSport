3b7a2d19caa38442e0b2016bd994e1c58705a5e8
EstefaMH <estefymoncaleano@gmail.com>
2023-09-06 10:51:41 -0500

    Admin add form

--- src/components/Admin/AdminAdd.js/AdminAddForm.js
+++ src/components/Admin/AdminAdd.js/AdminAddForm.js
@@ -1,37 +1,74 @@
-import React, { useEffect, useState } from "react";
-import GET_AllProducts from "../../../services/Products/GET_AllProducts";
-import axios from "axios";
+import React from "react";
+
 
 function AdminAddForm() {
- 
-  /*{orders &&
-        orders.map((product, i) => (
-          <div key={i}>
-            <p>{product.nombre}</p>
-            <p>{product.estado}</p>
-            <img
-              src={require(`../../../assets/img/${product.imagen}`)}
-              alt="img"
-            ></img>
-          </div>
-        ))}*/
-  
-  function onSubmit() {}
 
   return (
     <div>
-      
-
       <form onSubmit={onSubmit}>
-        <select>
+        <input
+          required
+          type="text"
+          placeholder="Referencia"
+          name="ref"
+          onChange={HandleChange}
+          value={product.ref}
+        />
+        <select required name='category' onChange={HandleChange}>
+          <option disabled>Categoria</option>
           <option value="1">Accesorios deportivos</option>
           <option value="2">Ropa deportiva</option>
         </select>
-        <input type="text" placeholder="Nombre del producto" />
-        <input type="text" placeholder="Precio de adquisición" />
-        <input type="text" placeholder="precio de venta" />
-        <input type="text" placeholder="Dimensiones/ tallaje" />
-        <textarea></textarea>
+        <select name='brand' onChange={HandleChange}>
+          <option disabled>Marca</option>
+          <option value="Kamila">Kamila</option>
+          <option value="Molten">Molten</option>
+        </select>
+        <input
+          type="text"
+          placeholder="Nombre del producto"
+          name="productName"
+          onChange={HandleChange}
+          value={product.productName}
+        />
+        <input
+          type="text"
+          placeholder="Precio de adquisición"
+          name="priceBuy"
+          onChange={HandleChange}
+          value={product.priceBuy}
+        />
+        <input
+          type="text"
+          placeholder="precio de venta"
+          name="priceSale"
+          onChange={HandleChange}
+          value={product.priceSale}
+        />
+
+        <input
+          type="text"
+          placeholder="Dimensiones/ tallaje"
+          name="size"
+          onChange={HandleChange}
+          value={product.size}
+        />
+
+        <textarea
+          name="description"
+          onChange={HandleChange}
+          value={product.description}
+        ></textarea>
+
+        <input
+          name="image"
+          type="file"
+          accept=".png"
+          value={product.image}
+          onChange={HandleChange}
+        />
+
+        <button>Agregar</button>
       </form>
     </div>
   );
