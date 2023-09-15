621b964112da1c944961fc1beafb44e398be75c9
Yany954 <yanygonzalezyepez@gmail.com>
2023-09-15 14:51:24 -0400

    merge

--- src/components/Admin/AdminNavbar/Navbar.js
+++ src/components/Admin/AdminNavbar/Navbar.js
@@ -1,8 +1,8 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { Button, Menu } from 'antd';
-import { pathRoutes } from '../../../routes/PathRoutes';
-import '../Admin.css';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { Button, Menu } from "antd";
+import { pathRoutes } from "../../../routes/PathRoutes";
+import "../Admin.css";
 import {
   AppstoreOutlined,
   ContainerOutlined,
@@ -11,7 +11,7 @@ import {
   MenuFoldOutlined,
   MenuUnfoldOutlined,
   PieChartOutlined,
-} from '@ant-design/icons';
+} from "@ant-design/icons";
 
 function getItem(label, key, icon, children, type, path) {
   return {
@@ -25,19 +25,29 @@ function getItem(label, key, icon, children, type, path) {
 }
 
 const items = [
-  getItem('Inventario', '1', <PieChartOutlined />, '', '', pathRoutes.adminAdd),
-  getItem('Añadir', '2', <DesktopOutlined />, '', '', pathRoutes.adminAdd),
-  getItem('Editar', '3', <ContainerOutlined />, '', '', pathRoutes.adminUpdate),
-  getItem('Borrar', '4', <MailOutlined />, [
-    getItem('Option 5', '5'),
-    getItem('Option 6', '6'),
-    getItem('Option 7', '7'),
-    getItem('Option 8', '8'),
-  ], '', pathRoutes.adminDelete),
-  getItem('Perfil', 'sub2', <AppstoreOutlined />, [
-    getItem('Option 9', '9'),
-    getItem('Option 10', '10'),
-    getItem('Submenu', 'sub3', null, [getItem('Option 11', '11'), getItem('Option 12', '12')]),
+  getItem("Inventario", "1", <PieChartOutlined />, "", "", pathRoutes.adminAdd),
+  getItem("Añadir", "2", <DesktopOutlined />, "", "", pathRoutes.adminAdd),
+  getItem("Editar", "3", <ContainerOutlined />, "", "", pathRoutes.adminUpdate),
+  getItem(
+    "Borrar",
+    "4",
+    <MailOutlined />,
+    [
+      getItem("Option 5", "5"),
+      getItem("Option 6", "6"),
+      getItem("Option 7", "7"),
+      getItem("Option 8", "8"),
+    ],
+    "",
+    pathRoutes.adminDelete
+  ),
+  getItem("Perfil", "sub2", <AppstoreOutlined />, [
+    getItem("Option 9", "9"),
+    getItem("Option 10", "10"),
+    getItem("Submenu", "sub3", null, [
+      getItem("Option 11", "11"),
+      getItem("Option 12", "12"),
+    ]),
   ]),
 ];
 
@@ -46,7 +56,7 @@ const Navbar = () => {
   const toggleCollapsed = () => {
     setCollapsed(!collapsed);
   };
-  
+
   const handleUpdate = () => {
     // Aquí puedes agregar tu código de backend si es necesario
   };
@@ -57,20 +67,17 @@ const Navbar = () => {
         {collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
       </Button>
       <Menu
-        defaultSelectedKeys={['1']}
-        defaultOpenKeys={['sub1']}
+        defaultSelectedKeys={["1"]}
+        defaultOpenKeys={["sub1"]}
         mode="inline"
         theme="light"
         inlineCollapsed={collapsed}
         items={items}
         className="admin-menu"
       >
-        {items.map(item => (
+        {items.map((item) => (
           <Menu.Item key={item.key}>
-
-            <Link to="/adminAdd">{item.label}</Link>
-
-
+            <Link to={item.path}>{item.label}</Link>
           </Menu.Item>
         ))}
       </Menu>
